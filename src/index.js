eae8ac2837c42d25a4ba99bf2e3316c73c21a393
Ryan Stimmler <rstimmler4@gmail.com>
2018-04-13 23:43:47 -0400

    formats with prettier

--- src/index.js
+++ src/index.js
@@ -1,70 +1,80 @@
-import React from 'react';
+import React from "react";
 import { Field, FastField } from "formik";
-import get from 'lodash.get';
+import get from "lodash.get";
 
 export const createField = (fields = {}) => {
   const {
-    input: Input = 'input', 
-    select: Select = 'select', 
-    textarea: TextArea = 'textarea',
-    checkbox: Checkbox = 'input',
-    formGroup: FormGroup = 'div', 
-    label: Label = 'label', 
-    helpText: HelpText = 'span',
-    selectPathSpec = 'target.value',
-    fastMode = false,
+    input: Input = "input",
+    select: Select = "select",
+    textarea: TextArea = "textarea",
+    checkbox: Checkbox = "input",
+    formGroup: FormGroup = "div",
+    label: Label = "label",
+    helpText: HelpText = "span",
+    selectPathSpec = "target.value",
+    fastMode = false
   } = fields;
 
-  const helpText = (name, touched, errors) => (touched[name] && errors[name]) && <HelpText variant="danger">{errors[name]}</HelpText>;
+  const helpText = (name, touched, errors) =>
+    touched[name] &&
+    errors[name] && <HelpText variant="danger">{errors[name]}</HelpText>;
 
-  const CustomFormGroup = ({ label, touched, errors, name, showLabel, children }) => (
+  const CustomFormGroup = ({
+    label,
+    touched,
+    errors,
+    name,
+    showLabel,
+    children
+  }) => (
     <FormGroup>
       {showLabel && <Label>{label}</Label>}
       {children}
       {helpText(name, touched, errors)}
     </FormGroup>
-  )
+  );
 
-  const CustomInput = ({ 
-    field, 
-    form, 
-    showLabel = true,
-    label,
-    ...props 
-  }) => (
-    <CustomFormGroup {...form} label={label} showLabel={showLabel} name={field.name}>
-      <Input 
-        {...field} 
-        {...props} 
-        label={label}
-      />
+  const CustomInput = ({ field, form, showLabel = true, label, ...props }) => (
+    <CustomFormGroup
+      {...form}
+      label={label}
+      showLabel={showLabel}
+      name={field.name}
+    >
+      <Input {...field} {...props} label={label} />
     </CustomFormGroup>
   );
 
-  const CustomTextarea = ({ 
-    field, 
-    form, 
+  const CustomTextarea = ({
+    field,
+    form,
     showLabel = true,
     label,
-    ...props 
+    ...props
   }) => (
-    <CustomFormGroup {...form} label={label} showLabel={showLabel} name={field.name}>
-      <TextArea 
-        {...field} 
-        {...props}
-        label={label}
-      />
+    <CustomFormGroup
+      {...form}
+      label={label}
+      showLabel={showLabel}
+      name={field.name}
+    >
+      <TextArea {...field} {...props} label={label} />
     </CustomFormGroup>
   );
 
   const CustomSelect = ({
-    field, 
-    form: { setFieldValue, ...formOthers  }, 
+    field,
+    form: { setFieldValue, ...formOthers },
     showLabel = true,
     label,
-    ...props 
+    ...props
   }) => (
-    <CustomFormGroup {...formOthers} label={label} showLabel={showLabel} name={field.name}>
+    <CustomFormGroup
+      {...formOthers}
+      label={label}
+      showLabel={showLabel}
+      name={field.name}
+    >
       <Select
         {...field}
         {...props}
@@ -73,20 +83,25 @@ export const createField = (fields = {}) => {
       />
     </CustomFormGroup>
   );
- 
+
   const CustomCheckBox = ({
-    field, 
-    form, 
+    field,
+    form,
     showLabel = true,
     label,
-    ...props 
+    ...props
   }) => (
-    <CustomFormGroup {...form} label={label} showLabel={showLabel} name={field.name}>
+    <CustomFormGroup
+      {...form}
+      label={label}
+      showLabel={showLabel}
+      name={field.name}
+    >
       <Checkbox
         {...field}
         {...props}
         label={label}
-        checked={get(form,`values.${field.name}`,false)}
+        checked={get(form, `values.${field.name}`, false)}
       />
     </CustomFormGroup>
   );
@@ -108,24 +123,15 @@ export const createField = (fields = {}) => {
     checkbox: CustomCheckBox,
     "datetime-local": CustomInput,
     select: CustomSelect,
-    textarea: CustomTextarea,
+    textarea: CustomTextarea
   };
 
-  const excludedFastTypes = [
-    'select' 
-  ];
+  const excludedFastTypes = ["select"];
 
   const FieldComponet = fastMode ? FastField : Field;
 
   return ({ type = "text", ...props }) => {
     const InputComponent = inputs[type];
-    return (
-      <FieldComponet 
-        component={InputComponent}
-        type={type}
-        {...props}
-      />
-    );
-  }
-
-}
+    return <FieldComponet component={InputComponent} type={type} {...props} />;
+  };
+};
